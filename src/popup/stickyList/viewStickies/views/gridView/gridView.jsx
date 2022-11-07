c9c68628df4d4d52356376b6d6fb8087dfdbf1b2
Sahil Daswani <sahildaswani18@gmail.com>
2022-11-07 17:05:36 +0800

    Add animation

--- src/popup/stickyList/viewStickies/views/gridView/gridView.jsx
+++ src/popup/stickyList/viewStickies/views/gridView/gridView.jsx
@@ -4,14 +4,17 @@ import Grid from "@mui/material/Unstable_Grid2";
 import Divider from "@mui/material/Divider";
 import Title from "../title";
 import StickyItem from "./stickyItem";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const GridView = ({ stickies, webpageTitles }) => {
+	const [animateSticky] = useAutoAnimate({ duration: 250 });
+
 	return (
 		<Box sx={{ my: 2 }}>
 			{Object.keys(stickies).map((url) => (
 				<>
 					<Title key={url} url={url} title={webpageTitles[url]} />
-					<Grid container spacing={1} columns={2} sx={{ my: 1 }}>
+					<Grid container spacing={1} columns={2} sx={{ my: 1 }} ref={animateSticky}>
 						{stickies[url].map((sticky) => (
 							<StickyItem key={sticky.id} url={url} sticky={sticky} />
 						))}
